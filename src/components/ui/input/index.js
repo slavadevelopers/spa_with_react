a13d1547e90ab93a9bf5eb24001ab621514a0a21
Vyacheslav Mukhin <slavadevelopers@gmail.com>
2017-07-02 21:46:26 +0300

    Lesson 6_6 complete

---
+++ src/components/ui/input/index.js
@@ -0,0 +1,50 @@
+import React  from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classname';
+
+export default class Input extends React.Component {
+
+    static propTypes = {
+        value: PropTypes.string.isRequired,
+        onChange: PropTypes.func.isRequired,
+        divClasses: PropTypes.string,
+        error: PropTypes.string
+    };
+
+    constructor(props) {
+        super(props);
+
+        const { value } = this.props;
+        this.state = { value };
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        const { value } = event.target;
+        this.props.onChange(value);
+        this.setState({ value });
+    }
+
+    render() {
+        const divClasses = classNames({
+            'form-control': true,
+            'form-control-danger': this.props.error ? true : false
+        });
+
+        const { value } = this.props;
+
+        return (
+            <div>
+                <input
+                    type='text'
+                    value={ value }
+                    onChange={ this.handleChange }
+                    className={ divClasses }
+                />
+                { this.props.error ? <div className='form-control-feedback'>{ this.props.error }</div> : null }
+            </div>
+        );
+    }
+
+}
--- src/page/home/home.js
+++ src/page/home/home.js
@@ -1,16 +1,79 @@
 import React from 'react';
+import Input from '../../components/ui/input/index';
+import { bindAll } from 'lodash';
+import './styles.less';
 
 export default class Home extends React.Component {
-    
+
     static path = '/';
-    
-    render() {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            todoName: '',
+            todoList: [
+                {
+                    id: 1,
+                    name: 'ToDo-1'
+                }
+            ],
+            error: ''
+        }
+
+        bindAll(this, ['renderTodo', 'onChangeInput', 'addTodo']);
+    }
+
+    onChangeInput(value) {
+        this.setState({ todoName: value });
+    }
+
+    addTodo() {
+        if (this.state.todoName === '') {
+            this.setState({ error: 'Нельзя добавить пустую запись' });
+            return;
+        };
+        const id = this.state.todoList[this.state.todoList.length - 1].id + 1;
+        const name = this.state.todoName;
+        const todoItem = { id, name }
+        const todoList = this.state.todoList;
+        todoList.push(todoItem);
+        this.setState({ todoName: '', error: '', todoList });
+    }
+
+    renderTodo(item) {
         return (
-            <div>
-                <div className='alert alert-success' role='alert'>
-                    <strong>Well done!</strong> You successfully read this important alert message.
+            <li key={ item.id }>{ item.name }</li>
+        );
+    }
+
+    render() {
+        const { todoName, todoList, error } = this.state;
+        return (
+                <div className='container-fluid'>
+                    <div className='row'>
+                        <div className='col-4'>
+                            <ul className='pl-15'>
+                                { todoList.map(this.renderTodo) }
+                            </ul>
+                            <div className='row'>
+                                <div className='col-8'>
+                                    <Input
+                                        value={ todoName }
+                                        onChange={this.onChangeInput}
+                                        error={ error }
+                                    />
+                                </div>
+                                <div className='col-4'>
+                                    <button
+                                        className='btn btn-primary w-100'
+                                        onClick={ this.addTodo }
+                                    >Add ToDo</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
         );
     }
 }
