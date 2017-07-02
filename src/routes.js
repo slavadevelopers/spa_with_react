a0ab61692d8e8003814b952c1c772297c6ed856c
Vyacheslav Mukhin <slavadevelopers@gmail.com>
2017-07-02 18:27:00 +0300

    restructuring App

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { HomeRoute } from './components/page/home/index';
-import { ContactRoute } from './components/page/contact/index';
-import ErrorPage from './components/page/errors/index';
+import { HomeRoute } from './page/home/index';
+import { ContactRoute } from './page/contact/index';
+import ErrorPage from './page/errors/index';
 
 
 export default (
